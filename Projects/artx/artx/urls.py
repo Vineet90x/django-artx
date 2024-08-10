67f627f9e84b860a51639f0a2db36e7b580623ad
alpha <vineetsalian42@gmail.com>
2024-08-10 08:01:47 +0530

    add

--- Projects/artx/artx/urls.py
+++ Projects/artx/artx/urls.py
@@ -19,10 +19,13 @@ from django.urls import path , include
 from django.conf import settings
 from django.conf.urls.static import static
 from .import views
+from django.contrib.auth.urls import views as auth_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('art/', include('art.urls')),
-   # path('',views.home),
+    path('accounts/',include('django.contrib.auth.urls')),
+
+
 ] + static (settings.MEDIA_URL, document_root =settings.MEDIA_ROOT)
 
